5d69c87159ca9b91565d825e5b5040aec50421bc
akshaygarg834 <akshaygarg834@gmail.com>
2020-04-16 17:04:32 +0530

    Chapter Fix

--- src/components/Chapter.js
+++ src/components/Chapter.js
@@ -19,15 +19,14 @@ const Chapter = props => {
         <i className="material-icons">{iconClassName}</i>
         <h6>{`${sequenceNO}. ${title}`}</h6>
       </div>
-      <div
-        className="collapsible-body align-left"
-        style={{ paddingLeft: "10px" }}
-      >
+      <div className="collapsible-body" style={{ paddingLeft: "10px" }}>
         <ul style={{ paddingLeft: "20px" }}>
           {chapterContent.map(topic => (
             <Topic {...topic} chapterSequenceNO={sequenceNO} />
           ))}
         </ul>
+
+        {type === "lesson" && "Lorem ipsum dolor sit amet."}
       </div>
     </li>
   );
